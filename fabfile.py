094671218a2a6f5c31e3b95b13efd412e2803e5b
Nitin Bhojwani <nbhojwani@vmware.com>
2018-05-29 09:19:13 +0530

    remove trailing whitespaces and fix the script

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,13 @@ def check(dirs=None):
             dirs = dirs.split(',')
             for directory in dirs:
                 if directory:
-                    directory = directory[:-
-                                          1] if directory[-1] == '/' else directory
-                    result = local('flake8 ' + str(directory) + '/*.py')
+                    directory = directory[:-1] \
+                        if directory[-1] == '/' else directory
+                    result = local(
+                        'find %s -name \'*.py\' -exec flake8 {} \;'
+                        % directory)
             return
-        result = local('flake8 .')
+        result = local('find . -name \'*.py\' -exec flake8 {} \;')
         if not result:
             print('Please fix the Errors')
         return
@@ -24,15 +26,16 @@ def check_files(files=None):
         if files:
             files = files.split(',')
             for file_name in files:
-                if not '.py' in file_name:
-                    print (file_name + " is not .py file")
+                if '.py' not in file_name:
+                    print(file_name + " is not .py file")
                     continue
                 result = local('flake8 ' + str(file_name))
             if not result:
                 print('Please fix the Errors')
             return
         else:
-            print('format - fab check_files:filepath/filename1.py,filename2.py')
+            print(
+                'format - fab check_files:filepath/filename1.py,filename2.py')
         return
 
 
@@ -46,13 +49,23 @@ def auto_clean(dirs=None):
                       ' -name "*.pyc" -exec rm -rf {} \;')
                 directory = directory[:-
                                       1] if directory[-1] == '/' else directory
-                result = local('autopep8 --in-place ' +
-                               str(directory) + '/*.py')
+                result = local(
+                    'find %s -name \'*.py\' -exec autopep8 --in-place {} \;'
+                    % directory)
+                print("removing trailing whitespaces...")
+                result = local(
+                    'find %s -name \'*.py\' -exec \
+                     sed -i "" -e s/[[:space:]]*$// {} \;' % directory)
             return
         else:
             print('removing .pyc files')
             local('find . -name "*.pyc" -exec rm -rf {} \;')
-            result = local('find . -name \'*.py\' -exec autopep8 --in-place '{}' \;')
+            result = local(
+                'find . -name \'*.py\' -exec autopep8 --in-place {} \;')
+            print("removing trailing whitespaces...")
+            result = local(
+                'find . -name \'*.py\' -exec\
+                 sed -i "" -e s/[[:space:]]*$// {} \;')
             if not result:
                 print('Please fix the Errors')
             return
@@ -64,9 +77,8 @@ def auto_clean_files(files=None):
         if files:
             files = files.split(',')
             for file_name in files:
-                if not '.py' in file_name:
-                    print (file_name + " is not .py file")
+                if '.py' not in file_name:
+                    print(file_name + " is not .py file")
                     continue
-                result = local('autopep8 --in-place ' +
-                               str(file_name))
+                local('autopep8 --in-place %s' % file_name)
             return
